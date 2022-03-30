9810e9c13e1f9f0206a75ddc65c1c3d47a83aa6c
VitalKr <vitalkr@mail.ru>
2022-03-30 16:41:54 +0300

    Доработка

--- src/main/kotlin/1_3_2.kt
+++ src/main/kotlin/1_3_2.kt
@@ -1,40 +1,46 @@
-fun main(args: Array<String>) {
+import kotlin.math.roundToInt
+import kotlin.system.exitProcess
+
+fun main() {
 
     println("Введите сумму перевода:")
     println("Введите рубли:")
     val rub = readln()
     println("Введите копейки:")
     val kop = readln()
-    println("Введите тип карты: (1, 2, 3)")
-    println("1. MasterCard и Maestro")
-    println("2. Visa и МИР")
-    println("3. VK Pay")
+    println("Введите тип карты: (1, 2, 3, 4, 5)")
+    println("1. MasterCard")
+    println("2. Maestro")
+    println("3. Visa")
+    println("4. МИР")
+    println("5. VK Pay")
     val typeRead = readln()
     println("Введите сумму предыдущих покупок за месяц в рублях:")
     val sumMonthRead = readln()
-    var amount_ru = 0
-    var amount_k = 0
-    var amount = 0
-    var sumMonth = 0
-    var type = 0
+    val amountRu: Int
+    val amountK: Int
+    val amount: Int
+    val sumMonth: Int
+    val type: Int
 
     try {
-        amount_ru = rub.toInt()
-        amount_k = kop.toInt()
+        amountRu = rub.toInt()
+        amountK = kop.toInt()
         type = typeRead.toInt()
-        val sumMonth_k = sumMonthRead.toInt()
-        sumMonth = sumMonth_k * 100
-        amount = amount_ru * 100 + amount_k
+        val sumMonthK = sumMonthRead.toInt()
+        sumMonth = sumMonthK * 100
+        amount = amountRu * 100 + amountK
         print("Сумма желаемого перевода равна:")
-        println("  " + amount_ru + " руб. " + amount_k + " коп.")
+        println("  $amountRu руб. $amountK коп.")
         println("______________________________________________________________")
     } catch (e: Exception) {
         println("Вы ввели не число")
+        exitProcess(-1)
     }
+
     if ((amount > 150_000_00) || (sumMonth > 600_000_00)) {
         println("Превышен Ваш лимит 150000 рублей в сутки или 600000 рублей в месяц")
     } else commission(type, sumMonth, amount)
-
 }
 
 fun commission(
@@ -43,13 +49,13 @@ fun commission(
     amount: Int
 ): Double {
     return when (type) {
-        1 -> {
+        1, 2 -> {
             mastercard(amount)
         }
-        2 -> {
+        3, 4 -> {
             visa(amount)
         }
-        3 -> {
+        5 -> {
             vk(amount, sumMonth)
         }
         else -> {
@@ -62,16 +68,15 @@ fun commission(
 private fun mastercard(amount: Int) = if (amount < 75000_00) {
     val perevod = (amount / 100.00)
     val procent1 = 0.0
-    print(perevod, procent1)
+    sumPrint(perevod, procent1)
     procent1
 } else {
     val procent = ((amount * (0.6 / 100)) + 20_00)
     val perevod = (amount + (procent)) / 100
     val procent1 = procent / 100
-    print(perevod, procent1)
+    sumPrint(perevod, procent1)
     procent
 }
-
 
 private fun visa(amount: Int) = if ((35_00 < amount) && (amount < 150_000_00)) {
     var procent = (amount * 0.75 / 100)
@@ -80,7 +85,7 @@ private fun visa(amount: Int) = if ((35_00 < amount) && (amount < 150_000_00)) {
     }
     val perevod = (amount + (procent)) / 100
     val procent1 = procent / 100
-    print(perevod, procent1)
+    sumPrint(perevod, procent1)
     procent
 
 } else {
@@ -94,13 +99,13 @@ private fun vk(amount: Int, sumMonth: Int) = if ((amount > 15000_00) || (40000_0
 } else {
     val perevod = (amount / 100.00)
     val procent1 = 0.0
-    print(perevod, procent1)
+    sumPrint(perevod, procent1)
     procent1
 }
 
-private fun print(perevod: Double, procent1: Double) {
-    println("Всего списано за перевод: ${Math.round(perevod * 100.0) / 100.0}")
-    println("Сумма комиссии за перевод: ${Math.round(procent1 * 100.0) / 100.0}")
+private fun sumPrint(perevod: Double, procent1: Double) {
+    println("Всего списано за перевод: ${(perevod * 100.0).roundToInt() / 100.0}")
+    println("Сумма комиссии за перевод: ${(procent1 * 100.0).roundToInt() / 100.0}")
 }
 
 
